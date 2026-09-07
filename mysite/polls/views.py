34c4099a3cddf9d9c007b119bd8487b43e1de685
Unna Mäntyniemi <unna.mantyniemi@helsinki.fi>
2026-09-07 12:59:08 +0300

    Add tests

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,13 +13,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """
-        Return the last five published questions (not including those set to be
-        published in the future).
+        Excludes any questions that aren't published yet.
         """
-        return Question.objects.filter(
-            pub_date__lte=timezone.now()
-        ).order_by('-pub_date')[:5]
-
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class DetailView(generic.DetailView):
     model = Question
